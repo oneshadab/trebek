bec4c45fafa06378ba40119b6a7e29ff3c42bfa4
Shadman Shadab <oneshadab@gmail.com>
2020-11-21 03:51:07 +0600

    Create sample program for parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod lang;
+
+use lang::parser::Parser;
+
+
 fn main() {
-    println!("Hello, world!");
+    let program = "
+        (+ 1 2)
+    ";
+
+    let parser = Parser{};
+    let output = parser.tokenize(program.into());
+
+    println!("{:?}", output);
 }
