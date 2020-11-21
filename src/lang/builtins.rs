1778705d6061c9d96136100bb62a96068c5432bd
Shadman Shadab <oneshadab@gmail.com>
2020-11-21 20:57:30 +0600

    Create def builtin

--- src/lang/builtins.rs
+++ src/lang/builtins.rs
@@ -1,15 +1,30 @@
-use super::scope::Scope;
+use super::{types::Record, scope::Scope};
 
-pub fn add(scope: &mut Scope, args: Vec<String>) -> String {
+pub fn add(scope: &mut Scope, args: Vec<String>) -> Record {
   match &args[..] {
     [a, b] => {
       let i_a: i32 = a.parse().unwrap();
       let i_b: i32 = b.parse().unwrap();
+      let i_result = i_a + i_b;
 
-      return (i_a + i_b).to_string();
+      return Record::Symbol(i_result.to_string());
     }
     _ => {
-      panic!("Missing args for function call")
+      panic!("Function called with incorrect number of args")
+    }
+  }
+}
+
+pub fn def(scope: &mut Scope, args: Vec<String>) -> Record {
+  match &args[..] {
+    [symbol, val] => {
+      let entry = Record::Symbol(val.clone());
+      scope.set(symbol.clone(), entry);
+
+      return Record::Empty;
+    }
+    _ => {
+      panic!("Function called with incorrect number of args")
     }
   }
 }
