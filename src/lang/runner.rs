f734f11a9d3ea7f2c12ee6f73153a6129f3e0863
Shadman Shadab <oneshadab@gmail.com>
2020-11-21 17:09:12 +0600

    Add default addition function

--- src/lang/runner.rs
+++ src/lang/runner.rs
@@ -1,4 +1,4 @@
-use super::scope::Scope;
+use super::{parser::Parser, scope::Scope};
 pub struct Runner {
   rootScope: Scope
 }
@@ -6,5 +6,23 @@ pub struct Runner {
 impl Runner {
   pub fn new(&mut self) {
     self.rootScope = Scope::new();
+
+    self.rootScope.set(
+      String::from('+'),
+      |args: Vec<String>| -> String {
+        match &args[..] {
+          [a, b] => {
+            let i_a: i32 = a.parse().unwrap();
+            let i_b: i32 = b.parse().unwrap();
+
+            return (i_a + i_b).to_string();
+          }
+          _ => {
+            panic!("Missing args for function call")
+          }
+        }
+      }
+    );
   }
+
 }
