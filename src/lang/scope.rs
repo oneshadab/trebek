4f0348b63666796ed03d69c7ca046bf56d3adbe2
Shadman Shadab <oneshadab@gmail.com>
2020-11-26 13:29:43 +0600

    Add lookup method

--- src/lang/scope.rs
+++ src/lang/scope.rs
@@ -17,7 +17,12 @@ impl Scope {
     self.records.insert(key, val);
   }
 
-  pub fn resolve(&mut self, key: &String) -> Option<&Record>{
-    return self.records.get(key);
+  pub fn resolve(&self, key: &String) -> Record {
+    let default = Record::Symbol(key.clone().into());
+    return self.lookup(key).unwrap_or(default);
+  }
+
+  pub fn lookup(&self, key: &String) -> Option<Record>{
+    return self.records.get(key).cloned();
   }
 }
