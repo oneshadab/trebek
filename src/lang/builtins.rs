47f4c1c6a3a1c855289bb430018ff49be9fa7172
Shadman Shadab <oneshadab@gmail.com>
2020-11-27 00:42:06 +0600

    Add builtin for adding new function

--- src/lang/builtins.rs
+++ src/lang/builtins.rs
@@ -1,4 +1,13 @@
-use super::{runner::Runner, types::{builtin::Builtin, record::Record}};
+use super::{types::function::Function, parser::Parser, runner::Runner, types::{builtin::Builtin, record::Record}};
+
+pub fn get_builtins() -> Vec<Builtin> {
+  vec![
+    Builtin::new("+", add),
+    Builtin::new("def", def),
+    Builtin::new("print", print),
+    Builtin::new("fn", new_function),
+  ]
+}
 
 pub fn add(ctx: &mut Runner, args: &[Record]) -> Record {
   match args {
@@ -51,10 +60,27 @@ pub fn print(ctx: &mut Runner, args: &[Record]) -> Record {
   }
 }
 
-pub fn get_builtins() -> Vec<Builtin> {
-  vec![
-    Builtin::new("+".into(), add),
-    Builtin::new("def".into(), def),
-    Builtin::new("print".into(), print),
-  ]
+pub fn new_function(ctx: &mut Runner, args: &[Record]) -> Record {
+  match args {
+    [Record::Expression(params_expr), Record::Expression(body)] => {
+      let mut parser = Parser::new();
+      let params = parser.tokenize_expression(&params_expr);
+
+      let qualified_params = params
+        .into_iter()
+        .map(|p| {
+          match p {
+            Record::Symbol(s) => { s },
+            other => { panic!("{:?} is not a proper param!", other) }
+          }
+        })
+        .collect();
+
+      let func = Function::new(qualified_params, body.into());
+      Record::Function(func)
+    }
+    _ => {
+      panic!("'print' called with incorrect number of args")
+    }
+  }
 }
