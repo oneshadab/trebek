9a4a3060308d6ad35955df636d3cbb7917c1dee4
Shadman Shadab <oneshadab@gmail.com>
2020-11-27 00:22:55 +0600

    Create builtin type

--- src/lang/types/builtin.rs
+++ src/lang/types/builtin.rs
@@ -1,5 +1,27 @@
+use std::fmt;
 use crate::lang::runner::Runner;
 
-use super::record::Record;
+use super::{record::Record, symbol::Symbol};
 
-pub type Builtin = fn(&mut Runner, &[Record]) -> Record;
+type ApplyFn = fn(&mut Runner, &[Record]) -> Record;
+
+#[derive(Clone)]
+pub struct Builtin {
+  pub name: &'static str,
+  pub apply: ApplyFn
+}
+
+impl Builtin {
+  pub fn new(name: &'static str, apply: ApplyFn) -> Builtin {
+    Builtin {
+      name,
+      apply
+    }
+  }
+}
+
+impl fmt::Debug for Builtin {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      f.debug_struct(&self.name[..]).finish()
+  }
+}
