eee264cc083604f9b8655f68e5899ff08ec5f719
Shadman Shadab <oneshadab@gmail.com>
2020-11-28 19:58:14 +0600

    Generate math functions

--- src/lang/builtins/math.rs
+++ src/lang/builtins/math.rs
@@ -4,30 +4,42 @@ use crate::lang::{runtime::Runtime, types::{builtin::Builtin, record::Record}};
 pub fn get_builtins() -> Vec<Builtin> {
   vec![
     Builtin::new("+", add),
+    Builtin::new("-", subtract),
+    Builtin::new("*", multiply),
+    Builtin::new("/", divide),
   ]
 }
 
-fn add(ctx: &mut Runtime, args: &[Record]) -> Record {
-  match args {
-    [arg, other_arg] => {
-      let val = ctx.eval(arg);
-      let other_val = ctx.eval(other_arg);
+macro_rules! create_math_function {
+  ($func_name: ident, $operation: tt) => {
+    fn $func_name(ctx: &mut Runtime, args: &[Record]) -> Record {
+      match args {
+        [arg, other_arg] => {
+          let val = ctx.eval(arg);
+          let other_val = ctx.eval(other_arg);
 
-      match (val, other_val) {
-        (Record::Symbol(a), Record::Symbol(b)) => {
-          let i_a: i32 = a.parse().unwrap();
-          let i_b: i32 = b.parse().unwrap();
-          let i_result = i_a + i_b;
+          match (val, other_val) {
+            (Record::Symbol(a), Record::Symbol(b)) => {
+              let i_a: i32 = a.parse().unwrap();
+              let i_b: i32 = b.parse().unwrap();
+              let i_result = i_a $operation i_b;
 
-          return Record::Symbol(i_result.to_string());
+              return Record::Symbol(i_result.to_string());
+            }
+            _ => {
+              panic!("'add' called with incorrect params")
+            }
+          }
         }
         _ => {
-          panic!("'add' called with incorrect params")
+          panic!("'add' called with incorrect number of args")
         }
       }
     }
-    _ => {
-      panic!("'add' called with incorrect number of args")
-    }
-  }
+  };
 }
+
+create_math_function!(add, +);
+create_math_function!(subtract, -);
+create_math_function!(multiply, *);
+create_math_function!(divide, /);
