4176e8f19fb24c17b45b6a7351d47cef24feb6fb
Shadman Shadab <oneshadab@gmail.com>
2020-11-29 00:59:53 +0600

    Add method for defining function

--- src/lang/builtins/function.rs
+++ src/lang/builtins/function.rs
@@ -4,32 +4,57 @@ use crate::lang::{types::function::Function, parser::Parser, runtime::Runtime, t
 
 pub fn get_builtins() -> Vec<Builtin>{
   vec![
-    Builtin::new("fn", new_function),
+    Builtin::new("fn", create_function),
+    Builtin::new("defn", define_function),
   ]
 }
 
-
-pub fn new_function(_ctx: &mut Runtime, args: &[Record]) -> Record {
+fn create_function(_ctx: &mut Runtime, args: &[Record]) -> Record {
   match args {
-    [Record::Expression(params_expr), Record::Expression(body)] => {
-      let mut parser = Parser::new();
-      let params = parser.tokenize_expression(&params_expr);
-
-      let qualified_params = params
-        .into_iter()
-        .map(|p| {
-          match p {
-            Record::Symbol(s) => { s },
-            other => { panic!("{:?} is not a proper param!", other) }
-          }
-        })
-        .collect();
-
-      let func = Function::new(qualified_params, body.into());
+    [
+      Record::Expression(params_expr),
+      Record::Expression(body_expr)
+    ] => {
+      let func = init_function(params_expr, body_expr);
       Record::Function(func)
     }
     _ => {
       panic!("'print' called with incorrect number of args")
     }
   }
+}
+
+fn define_function(ctx: &mut Runtime, args: &[Record]) -> Record {
+  match args {
+    [
+      Record::Symbol(symbol),
+      Record::Expression(params_expr),
+      Record::Expression(body_expr),
+    ] => {
+      let func = init_function(params_expr, body_expr);
+      ctx.set_global(symbol.clone(), Record::Function(func));
+
+      Record::Empty
+    }
+    _ => {
+      panic!("'defn' called with incorrect number of args")
+    }
+  }
+}
+
+fn init_function(params_expr: &String, body: &String) -> Function {
+  let mut parser = Parser::new();
+  let params = parser.tokenize_expression(params_expr);
+
+  let qualified_params = params
+    .into_iter()
+    .map(|p| {
+      match p {
+        Record::Symbol(s) => { s },
+        other => { panic!("{:?} is not a proper param!", other) }
+      }
+    })
+    .collect();
+
+  Function::new(qualified_params, body.into())
 }
--- src/lang/builtins/scope.rs
+++ src/lang/builtins/scope.rs
@@ -3,16 +3,16 @@ use crate::lang::{runtime::Runtime, types::{builtin::Builtin, record::Record}};
 
 pub fn get_builtins() -> Vec<Builtin>{
   vec![
-    Builtin::new("def", def),
+    Builtin::new("def", define),
   ]
 }
 
-pub fn def(ctx: &mut Runtime, args: &[Record]) -> Record {
+pub fn define(ctx: &mut Runtime, args: &[Record]) -> Record {
   match args {
     [Record::Symbol(symbol), val] => {
       ctx.set_global(symbol.clone(), val.clone());
 
-      return Record::Empty;
+      Record::Empty
     }
     _ => {
       panic!("'def' called with incorrect number of args")
