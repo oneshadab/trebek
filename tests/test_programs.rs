f3359e1509a2110e0c8ae6134858d8c3274d41b3
Shadman Shadab <oneshadab@gmail.com>
2020-11-28 18:50:47 +0600

    Add runtime test

--- tests/test_programs.rs
+++ tests/test_programs.rs
@@ -1,15 +1,29 @@
-
 #![cfg(test)]
 extern crate test_generator;
 
 pub mod tests {
-  use trebek::lang::runtime::Runtime;
+  use std::{path::Path, fs::read_to_string};
+
   use test_generator::test_resources;
+
+  use trebek::lang::runtime::Runtime;
 
 
   #[test_resources("tests/res/programs/*")]
   fn verify(program_dir: &str) {
-    println!("[DBG] {}", program_dir);
-    assert!(std::path::Path::new(program_dir).exists());
+    let dir = Path::new(program_dir);
+
+    let program_path = dir.join("program.tr");
+    let program = read_to_string(program_path).expect("Program not found!");
+
+    let expected_output_path = dir.join("expected_output.txt");
+    let expected_output = read_to_string(expected_output_path).expect("Expected output not found!");
+
+
+    let mut runtime = Runtime::new();
+    let output = runtime.run(program);
+
+
+    assert_eq!(output, expected_output);
   }
 }
